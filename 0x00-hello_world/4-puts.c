54182a829e71d5ee2d39e75cf62dc09be20e7e59
--add <oladelejoseph100@gmail.com>
2023-02-16 14:08:30 -0800

    Put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *  Description: main - prints out a string to stdout.
- *  Reture: 0 if succes.
- */
-int main (void)
+ *  * Description: main - Prints out a string to stdout.
+ *   * Return: 0 if success.
+ *    */
+int main(void)
 {
-puts ("\"programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
