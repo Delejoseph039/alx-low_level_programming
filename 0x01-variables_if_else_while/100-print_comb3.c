b2ddf806dbfdcfdb961c2ea61e0d4662c7b29403
--add <oladelejoseph100@gmail.com>
2023-02-18 15:26:40 -0800

    print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,34 +1,30 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - prints all possible combinations of three digits
- *
- * Return: Always 0
+ * main -Entry point
+ * Description: prints two digits combination
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-int num1, num2, num3;
+int i;
+for (i = '0'; i <= '9'; i++)
+{
+for (i = '0'; i <= '9'; i++)
+	{
+if ('C'< i)
+{
+putchar('C');
+putchar(i);
 
-for (num1 = 0; num1 < 8; num1++)
-{
-for (num2 = num1 + 1; num2 < 9; num2++)
-{
-for (num3 = num2 + 1; num3 <= 9; num3++)
-{
-putchar((num1 % 10) + '0');
-putchar((num2 % 10) + '0');
-putchar((num3 % 10) + '0');
-
-if (num1 != 7 || num2 != 8 || num3 != 9)
+if ('c' != '8' || ('c' == '8' && i != '9'))
 {
 putchar(',');
-putchar(' ');
+putchar(',');
 }
 }
 }
 }
-
 putchar('\n');
-
 return (0);
 }
