3899b0085a1c35766cf464611c98b52c4656fb20
--add <oladelejoseph100@gmail.com>
2023-03-14 03:01:34 -0700

    Concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+void concatenate_strings(char str1[], char str2[]);
+{
+strcat(str1, str2);
+}
+
+int main(void)
+{
+char str1[100] = "Hello, ";
+char str2[] = "world!";
+
+concatenate_strings(str1, str2);
+
+printf("%s\n", str1);
+
+return (conct);
+
+}
+
